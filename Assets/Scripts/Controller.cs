e26cb673dcfeb5900ff1a1a5ccba58ab7ec11f91
Jukka Lnsineva <jukka.lansineva@dnainternet.net>
2014-12-19 17:46:55 +0200

    some

---
+++ Assets/Scripts/Controller.cs
@@ -0,0 +1,19 @@
+﻿using UnityEngine;
+using System.Collections;
+using System.Collections.Generic;
+
+public class Controller : MonoBehaviour {
+
+	Dictionary<string, bool> actions;
+	// Use this for initialization
+	void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		if(actions["Fire1"]) {
+
+		}
+	}
+}
